011e4bfad4aa0bf02b5e06ab8b4e37a629dffe01
Axel Rueda <axelcuer@gmail.com>
2022-07-05 18:08:34 -0300

    changes to ejer 1

--- main.py
+++ main.py
@@ -5,53 +5,49 @@ from activation_funcs import escalon
 
 def ejer1():
   # cargamos archivos y valores principlaes
-  data = np.genfromtxt('./data/gtp1/XOR_trn.csv',delimiter=',')
+  data = np.genfromtxt('./data/gtp1/OR_trn.csv',delimiter=',')
   Neurona = Perceptron(data,0.05)
-  max_num_epoc = 50
-  sencitivity_min = 0.8
 
-  # epocas de entrenamiento 
+  ## ENTRENAMIENTO -------------------------------------------------------------
+  max_num_epoc = 50
   epoc = 0
-  sencitivity = 0
-  while(epoc<max_num_epoc and sencitivity<sencitivity_min):
-    sencitivity = 0
+  umbral_error = 0.15
+  N = Neurona.data_trn.shape[0]
+
+  while(epoc<max_num_epoc):
+    e_trn = 0
     epoc+=1
-    for i in range(Neurona.data_trn.shape[0]):
-      y = Neurona.eval(i)
+    # entrenamos la neurona
+    for i in range(N):
+      y = Neurona.eval(i,escalon)
       Neurona.learning(i,y)
-      if escalon(y) == data[i,-1]:
-        sencitivity+=1
-    sencitivity /= data.shape[0]
-    print(f'epoca [{epoc}] sencivilidad del {sencitivity}%')
+    
+    for i in range(N):
+      y = Neurona.eval(i,escalon)
+      if (y != Neurona.yd[i]): e_trn+=1
+
+    e_trn/=N
+    print(f'el error en epoca [{epoc}] es del {e_trn}%')
+    if e_trn<umbral_error: break
   
-  # chequeamos que el clasificador funcione
+  ## COMPROBACION --------------------------------------------------------------
+  # funcion para graficar recta
   recta = lambda x: (-Neurona.weights[0]*x+Neurona.weights[2])/Neurona.weights[1]
 
-  data2 = np.genfromtxt('./data/gtp1/XOR_tst.csv',delimiter=',')
-  fig, ax = plt.subplots()
-  t_pos = 0
-  f_neg = 0
-  t_neg = 0
-  f_pos = 0
+  # cargamos dataset de test
+  data2 = np.genfromtxt('./data/gtp1/OR_tst.csv',delimiter=',')
+  e_tst = 0
   for i in range(data2.shape[0]):
-    y = Neurona.check(data2[i])
-    if y < 0 and data2[i,-1] == -1:
-      t_neg += 1
-    elif y < 0 and data2[i,-1] != -1:
-      f_neg += 1
-    elif y > 0 and data2[i,-1] == 1:
-      t_pos += 1
-    else:
-      f_pos += 1
-
-    color = 'ob' if y==data2[i,-1] else 'or'
-    ax.plot(data2[i,0],data2[i,1],color,markersize=2)
-  ax.plot(np.array([-1,1]),np.array([recta(-1),recta(1)]))
-  # print(np.array([-1,1]),np.array([recta(-1),recta(1)]))
-  plt.show()
-  print(f'datos [tp:{t_pos}] [fp:{f_pos}] [tn:{t_neg}] [fn:{f_neg}]')
+    y = Neurona.check(data2[i],escalon)
+    if y != Neurona.yd[i]: e_tst+=1
+  e_tst /= data2.shape[0]
+  print(f'el error con dataset test es del {e_tst}%')
+    
+  # fig, ax = plt.subplots()
+  # ax.plot(np.array([-1,1]),np.array([recta(-1),recta(1)]))
+  # ax.plot(np.array([-1,1]),np.array([recta(-1),recta(1)]))
+  # plt.show()
   
-
 
 if __name__=='__main__':
   ejer1()
