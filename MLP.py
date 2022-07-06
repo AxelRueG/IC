b2a4ef182a045fd58cec232988f4e5951b87510f
Axel Rueda Gimenez <57848514+AxelRueG@users.noreply.github.com>
2022-07-06 12:22:57 -0300

    started implement of mlp

---
+++ MLP.py
@@ -0,0 +1,39 @@
+import numpy as np 
+
+
+class MultiLayerPerceptron():
+    def __init__(self,architecture,data):
+        self.architecture = architecture
+        self.N = len(self.architecture)
+        self.x = data[:,0:-1].copy()
+        self.yd = data[:,-1].copy()
+        self.weights = []
+
+        # weights with bias as a input
+        l_inputs = self.x.shape[0]+1
+        for layer in self.architecture:
+            # matrix m,n where m = amount of neurons and n = amount of inputs 
+            self.weights.append(np.random.rand(layer,l_inputs)-0.5)
+            l_inputs = layer+1
+        
+    def eval(self,x_in):
+        y = []
+        x = x_in.copy()
+        for i in range(self.N):
+            # the first element is the bias with value -1
+            x = np.concatenate((np.array([-1]),x))
+            y.append(np.dot(self.weights[i],x))
+            # the new input is the output of the last layer
+            x = y[-1].copy()
+        return y
+
+    def forward_propagation(self,it):
+        return self.eval(self.x[it])
+        
+    def backward_propagation(self,it):
+        pass
+
+
+
+        
+
