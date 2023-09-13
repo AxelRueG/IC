8c3f4298a12c373dfa07782afcc9d09324e9081e
axelrueg <axelcuer@gmail.com>
2023-09-13 16:55:05 -0300

    ejercicio 3

--- ejercicios/guia3.py
+++ ejercicios/guia3.py
@@ -12,6 +12,26 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 
+# obtener modelo
+def get_modelo(type_model="mlp", n_neighbors=3, rs=0, pb=False):
+    
+    if type_model =="mlp":
+        # return MLPClassifier(hidden_layer_sizes=(64), max_iter=1000, random_state=rs)
+        return MLPClassifier(max_iter=1000, random_state=rs)
+    elif type_model == "nb":
+        return GaussianNB()
+    elif type_model == "lda":
+        return LinearDiscriminantAnalysis()
+    elif type_model == "kn":
+        return KNeighborsClassifier(n_neighbors)
+    elif type_model == "dt":
+        return DecisionTreeClassifier(random_state=rs)
+    elif type_model == "svm":
+        return svm.SVC(probability=pb, kernel="rbf")
+    else:
+        raise "modelo no valido"
+
+
 # Cargar el conjunto de datos Digits
 def ejer1():
     digits = load_digits()
@@ -25,9 +45,6 @@ def ejer1():
     # mlp = MLPClassifier(hidden_layer_sizes=(64), max_iter=1000, random_state=42)
 
     # Entrenar el modelo en el conjunto de entrenamiento
-    # print(f"numero de capas: {mlp.n_layers_}")
-    # print(f"numero de saldias: {mlp.n_outputs_}")
-
     mlp.fit(X_train, y_train)
     # print(f"numero de neuronas por capas: {[ i.shape for i in mlp.coefs_ ]}")
     print(f"numero de saldias: {mlp.classes_}")
@@ -41,37 +58,13 @@ def ejer1():
     print(f'Tasa de acierto en el conjunto de prueba: {accuracy:.2f}')
 
 
-def ejer1_kfold(k=5, modelo="mlp"):
-    # data = load_digits()
-    data = load_wine()
+def ejer2(k=5, type_model="mlp", n_neighbors=3, rs=0):
+    data = load_digits()
     X, y = data.data, data.target
 
-    model = None
+    model = get_modelo(type_model, n_neighbors, rs)
 
-    if modelo =="mlp":
-        model = MLPClassifier(hidden_layer_sizes=(64), max_iter=1000, random_state=42)
-    elif modelo == "nb":
-        model = GaussianNB()
-    elif modelo == "lda":
-        model = LinearDiscriminantAnalysis()
-    elif modelo == "kn":
-        model = KNeighborsClassifier(n_neighbors=3)
-    elif modelo == "dt":
-        model = DecisionTreeClassifier(random_state=0)
-    elif modelo == "svm":
-        model = svm.SVC()
-    elif modelo == "bagging":
-        model = BaggingClassifier(
-            base_estimator=KNeighborsClassifier(n_neighbors=3),
-            n_estimators=10,
-            max_samples=0.3
-        )
-    elif modelo == "adaboost":
-        model = AdaBoostClassifier()
-    else:
-        raise "modelo no valido"
     # kf = KFold(n_splits=k)
-
     # porcentaje_acierto = []
     
     # print(kf)
@@ -79,7 +72,7 @@ def ejer1_kfold(k=5, modelo="mlp"):
     #     X_train, y_train = X[train_index], y[train_index]
     #     X_test, y_test = X[test_index], y[test_index]
 
-    #     # Entrenar el modelo en el conjunto de entrenamiento
+    #     # Entrenar el type_model en el conjunto de entrenamiento
     #     mlp.fit(X_train, y_train)
 
     #     # Evaluar el rendimiento en el conjunto de prueba
@@ -87,23 +80,54 @@ def ejer1_kfold(k=5, modelo="mlp"):
     #     porcentaje_acierto.append(accuracy)
     #     print(f'Tasa de acierto en el k: {i} el conjunto de prueba: {accuracy:.2f}')
         # Validación cruzada con KFold
+    # print(f'Media tasa de acierto: {np.mean(porcentaje_acierto)} desvio: {np.std(porcentaje_acierto)}')
     
-    kfold = KFold(n_splits=k, shuffle=True, random_state=42)
+    kfold = KFold(n_splits=k, shuffle=True)
     accuracies = cross_val_score(model, X, y, cv=kfold)
     mean_accuracy = np.mean(accuracies)
     variance_accuracy = np.var(accuracies)
-    print(f'precision: {accuracy_score(y, model.predict(X))}')
-    # print(f'{k} particiones - Mean Accuracy: {mean_accuracy:.2f}, Variance: {variance_accuracy:.4f}')
+    print('-'*30)
+    print(f'[{type_model}]: {k} particiones -> Accuracy: {accuracies}')
+    print(f'Mean Accuracy: {mean_accuracy:.2f}, Variance: {variance_accuracy:.4f}')
     
-    # print(f'Media tasa de acierto: {np.mean(porcentaje_acierto)} desvio: {np.std(porcentaje_acierto)}')
 
-# ejer1()
+def ejer3(k=5, type_model="mlp", n_neighbors=3, rs=0):
+    data = load_wine()
+    X, y = data.data, data.target
 
-# ejer1_kfold(k=5, modelo="mlp")
-# ejer1_kfold(k=5, modelo="nb")
-# ejer1_kfold(k=5, modelo="lda")
-# ejer1_kfold(k=5, modelo="kn")
-# ejer1_kfold(k=5, modelo="dt")
-# ejer1_kfold(k=5, modelo="svm")
-ejer1_kfold(k=5, modelo="bagging")
-# ejer1_kfold(k=5, modelo="adaboost")
+    # meta_model = BaggingClassifier(
+    #     estimator=get_modelo(type_model,n_neighbors,rs),
+    #     n_estimators=10,
+    #     max_samples=0.32
+    # )
+
+    meta_model = AdaBoostClassifier(
+        estimator=get_modelo(type_model,n_neighbors,rs,pb=True),
+        n_estimators=10
+    )
+
+    kfold = KFold(n_splits=k, shuffle=True)
+    accuracies = cross_val_score(meta_model, X, y, cv=kfold)
+    mean_accuracy = np.mean(accuracies)
+    variance_accuracy = np.var(accuracies)
+    print('-'*30)
+    print(f'[{type_model}]: {k} particiones -> Accuracy: {accuracies}')
+    print(f'Mean Accuracy: {mean_accuracy:.2f}, Variance: {variance_accuracy:.4f}')
+
+
+
+if __name__ == '__main__':
+
+    # ejer1()
+
+    # ejer2(k=5, type_model="mlp", rs=42)
+    # ejer2(k=5, type_model="nb")
+    # ejer2(k=5, type_model="lda")
+    # ejer2(k=5, type_model="kn")
+    # ejer2(k=5, type_model="dt")
+    # ejer2(k=5, type_model="svm")
+
+    ejer3(k=5, type_model="nb")
+    ejer3(k=5, type_model="dt")
+    ejer3(k=5, type_model="svm")
+    # k-nearest neighbours no es un estimador base valido para adaboost
