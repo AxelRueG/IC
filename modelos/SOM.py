538149404f9c4d82f94654a486bb2a20e13ad62a
axelrueg <axelcuer@gmail.com>
2023-10-01 13:15:53 -0300

    animando som

--- modelos/SOM.py
+++ modelos/SOM.py
@@ -1,3 +1,4 @@
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import matplotlib.pyplot as plt
 from os.path import abspath
@@ -8,6 +9,8 @@ class SOM:
         self.n_input = n_input              # Dimensión de los datos de entrada
         self.map_dim = map_dim              # Dimensión del mapa SOM (defoult 10x10)
         self.pesos = np.random.rand(map_dim[0], map_dim[1], n_input)-0.5
+
+        # self.etapas_titulo = ['ajuste topologico',]
 
     # --- nos devuelve el indice para la matriz ------------------------------------------
     def encontrar_neurona_ganadora(self, x):
@@ -23,50 +26,58 @@ class SOM:
     def actualizar_vecindad(self, index, x, k, coef_learn):
         for i in range(-k,k+1):
             for j in range(-k,k+1):
-                if abs(i)+abs(j) < k:
+                if abs(i)+abs(j) <= k:
                     try: 
                         self.pesos[index[0]+i,index[1]+j] += (coef_learn * ( x - self.pesos[index] ))
                     except:
                         pass
 
     # --- Entrenamiento ------------------------------------------------------------------
-    def trn(self, data_set, vecindad = 5, epocs = [1000,1000,500], coef_learn=[0.1, 0.01]):
-        # Entrenamiento del SOM ajuste grueso
-        for _ in range(epocs[0]):
-            for x in data_set:
-                # Encontrar la neurona ganadora
-                ganadora = self.encontrar_neurona_ganadora(x)
-                self.actualizar_vecindad(ganadora, x, vecindad, coef_learn[0])
-        
-        # reduccion lineal
-        print('empieza a reducir linealmente el coef de aprendizaje')
-        coefs = np.linspace(coef_learn[0],coef_learn[1],epocs[1])
-        vecindades = np.linspace(vecindad, 0, epocs[1]).astype(int)
-        for epoc in range(epocs[1]):
-            for x in data_set:
-                # Encontrar la neurona ganadora
-                ganadora = self.encontrar_neurona_ganadora(x)
-                self.actualizar_vecindad(ganadora, x, vecindades[epoc], coefs[epoc])
-        
-        # Entrenamiento del SOM ajuste fino
-        print('empieza ajuste fino')
-        for _ in range(epocs[2]):
-            for x in data_set:
-                # Encontrar la neurona ganadora
-                ganadora = self.encontrar_neurona_ganadora(x)
-                self.actualizar_vecindad(ganadora, x, 0, coef_learn[1])
+    def trn(self, data_set, vecindad = 5, epocs = [1000,1000,500], coef_learn=[0.1, 0.01]):        
+        coef_learn.insert(1,0)
+        vecindad = [vecindad, 0, 0]
 
-    def plot_pesos(self):
-        for i in range(self.pesos.shape[0]):
-            for j in range(self.pesos.shape[1]):
-                plt.plot(self.pesos[i,j,0], self.pesos[i,j,1], '*k')
+        peso_history = [np.copy(self.pesos)]
+
+        for etapa in range(3):
+            for epoc in range(epocs[etapa]):
+                if etapa == 1:
+                    coefs = np.linspace(coef_learn[0],coef_learn[1],epocs[1])
+                    vecindades = np.linspace(vecindad[etapa], 0, epocs[1]).astype(int)
+                else:
+                    coefs = coef_learn[etapa] * np.ones(epocs[etapa])
+                    vecindades = vecindad[etapa] * np.ones(epocs[etapa]).astype(int)
+
+                for x in data_set:
+                    # Encontrar la neurona ganadora
+                    ganadora = self.encontrar_neurona_ganadora(x)
+                    # actulizar ganadora y vecidad
+                    self.actualizar_vecindad(ganadora, x, vecindades[epoc], coefs[epoc])
+                    # self.actualizar_vecindad_cuadrada(ganadora, x, vecindades[epoc], coefs[epoc])
+                    
+                    peso_history.append(np.copy(self.pesos))
+
+        
+        return peso_history
+
+    def plot_pesos(self, pesos):
+        for i in range(pesos.shape[0]):
+            for j in range(pesos.shape[1]):
+                plt.plot(pesos[i,j,0], pesos[i,j,1], '*k')
                 # Graficar conexiones
-                if i+1 < self.pesos.shape[0]:
-                    plt.plot(self.pesos[i:i+2,j,0], self.pesos[i:i+2,j,1], '-k')    
-                if j+1 < self.pesos.shape[1]:
-                    plt.plot(self.pesos[i,j:j+2,0], self.pesos[i,j:j+2,1], '-k')
+                if i+1 < pesos.shape[0]:
+                    plt.plot(pesos[i:i+2,j,0], pesos[i:i+2,j,1], '-k')    
+                if j+1 < pesos.shape[1]:
+                    plt.plot(pesos[i,j:j+2,0], pesos[i,j:j+2,1], '-k')
 
-        plt.show()
+    def animaccion(self, frame, pesos, x):
+        plt.clf()
+        plt.title(f'Frame {frame}')
+        plt.plot(x[:,0], x[:,1], '.r')
+        self.plot_pesos(pesos[frame])
+
+
+
 
 
 if __name__=='__main__':
@@ -76,9 +87,9 @@ if __name__=='__main__':
 
     som = SOM(2, (2,2))
 
-    som.plot_pesos()
-    som.trn(x, 1, [100,100,100])
+    pesos = som.trn(x, 0, [100,100,100])
+    # print(len(pesos))
 
-    for val in x:
-        plt.plot(val[0], val[1], '.r')
-    som.plot_pesos()
+    fig = plt.figure()
+    anim = FuncAnimation(fig, som.animaccion, frames=len(pesos), interval=10, fargs=(pesos, x))
+    plt.show()
